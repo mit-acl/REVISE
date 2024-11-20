cc88de430e02a0c55bc7fbfb669cdb6b093fa4b9
Alex Meredith <alexm0802@gmail.com>
2024-11-20 13:54:42 -0500

    pushing docs

---
+++ docs/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+import sphinx_rtd_theme
+sys.path.insert(0, os.path.abspath('../scripts/'))
+
+project = 'REVISE'
+copyright = '2024, Alex Rose'
+author = 'Alex Rose'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon', 'sphinx.ext.githubpages', 'sphinx.ext.viewcode', 'sphinx_rtd_theme']
+
+templates_path = ['_templates']
+exclude_patterns = ['Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
